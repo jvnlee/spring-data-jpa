07cdf8a7208c0c4cc84d69aee554ba8a2937d7ab
jvnlee <lhjun1028@naver.com>
2023-02-28 03:51:51 +0900

    feat: Spring Data JPA 방식으로 auditing 데이터용 BaseEntity 생성 및 적용
    메인 클래스 SpringdatajpaApplication에 @EnableJpaAuditing을 추가해서 Spring Data JPA 방식의 auditing 활성화. BaseEntity에는 @EntityListeners로 JPA 이벤트 리스너 클래스인 AuditingEntityListener를 등록해놓음. 해당 클래스가 auditing 이벤트 발생 시, 관련 데이터를 가져와 필드 어노테이션에 맞춰 데이터를 주입해줌.
    
    시간 데이터는 @CreatedDate와 @LastModifiedDate으로 명시해서 데이터를 주입받음. 행위자 데이터는 @CreatedBy와 @LastModifiedBy로 명시하는데, 행위자 정보는 AuditorAware 빈으로부터 제공받음.

--- src/main/java/learn/springdatajpa/SpringdatajpaApplication.java
+++ src/main/java/learn/springdatajpa/SpringdatajpaApplication.java
@@ -2,12 +2,30 @@ package learn.springdatajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringdatajpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdatajpaApplication.class, args);
+	}
+
+	@Bean
+	public AuditorAware<String> auditorAware() {
+		/*
+		등록이나 수정이 발생할 때 마다 auditorAware()가 호출되어 행위의 주체가 누군지 반환하고,
+		반환된 값은 BaseEntity의 createdBy와 lastModifiedBy의 값으로 주입됨
+
+		예시로 랜덤 UUID를 사용했지만, 실제로는 사용자 세션 등으로부터 신원 데이터를 가져와 사용함
+		 */
+		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 
 }
